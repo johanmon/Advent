175326e4020c2651b52d605b8460a5138941ab1e
Johan Montelius <johanmon@kth.se>
2022-12-02 09:02:14 +0100

    day 2 done

--- 2022/adv.ex
+++ 2022/adv.ex
@@ -35,4 +35,79 @@ defmodule Adv do
     e1 + e2 + e3
   end
 
+  ## Day 2 Fairly simple and the only tricky part is kepping track of
+  ## if A is rock or scissors. I simply started with cretaing a
+  ## sequence of tuples {:A, :Y} etc instead of representing them as
+  ## {:rock, :scissors}, which would have made life easier. 
+  ## Writing uo the rules was fine once the pattern is clear.
+
+
+  def day2() do
+    File.stream!("day2.csv") |>
+      Stream.map( fn (r) ->
+	[one, two] = String.split(r)
+	{String.to_atom(one), String.to_atom(two)}
+      end)
+  end
+
+  def day2a() do
+    day2() |>
+      Enum.reduce(0, fn (turn, n) -> playa(turn) + n end)
+  end
+
+  def day2b() do
+    day2() |>
+      Enum.reduce(0, fn (turn, n) -> playb(turn) + n end)
+  end  
+
+  def playa(turn) do
+    case turn do
+      {:A, :X} ->
+	1 + 3
+      {:A, :Y} ->
+	2 + 6
+      {:A, :Z} ->
+	3 + 0
+
+      {:B, :X} ->
+	1 + 0
+      {:B, :Y} ->
+	2 + 3
+      {:B, :Z} ->
+	3 + 6
+
+      {:C, :X} ->
+	1 + 6 
+      {:C, :Y} ->
+	2 + 0
+      {:C, :Z} ->
+	3 + 3
+    end
+  end
+
+  def playb(turn) do
+    case turn do
+      {:A, :X} ->
+	3 + 0
+      {:A, :Y} ->
+	1 + 3
+      {:A, :Z} ->
+	2 + 6
+
+      {:B, :X} ->
+	1 + 0
+      {:B, :Y} ->
+	2 + 3
+      {:B, :Z} ->
+	3 + 6
+
+      {:C, :X} ->
+	2 + 0 
+      {:C, :Y} ->
+	3 + 3
+      {:C, :Z} ->
+	1 + 6
+    end
+  end
+  
 end
