2d01a3a01e7be7f286f620a1da63ca5bf5ba9bf0
Johan Montelius <johanmon@kth.se>
2022-12-19 15:37:53 +0100

    day 16 did terminate but with wroing answer, starting from scratch, day 17 does trim the cave but something else needs to be done

--- 2022/day17.ex
+++ 2022/day17.ex
@@ -11,29 +11,33 @@ defmodule Day17 do
   end
 
   def task_a(rocks) do
-    wind = sample()
-    cave = MapSet.new([{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7}])
-    tetris(0, rocks, 0, cave, {wind, wind})
+    wind = wind(input())
+    cave = cave()    
+    {n, floor, cave} = tetris(0, rocks, 0, 0, cave, wind)
+    print(cave, n, floor)
+    {n, floor}
   end
 
-  def tetris(rocks, rocks, n, _cave, _) do
-    n
+  def tetris(rocks, rocks, n, floor, cave, _) do
+    {n, floor, cave}
   end
-  def tetris(rks, rocks, n, cave, wind) do
+  def tetris(rks, rocks, n, floor, cave, wind) do
     ## :io.format(" rks = ~w  n = ~w\n", [rks,n])
-    {n, cave, wind} = (rock(rks)).({n+4,3}, n, cave, wind)
-    ## print(cave, n)
-    tetris(rks+1, rocks, n, cave, wind)
+    {n, cave, wind} = (rock(rks)).({n+4,3}, n, floor, cave, wind)
+    {floor, cave} = trim(n, floor, cave)
+    #:io.format(" n = ~w floor = ~w\n", [n, floor])
+    #:io.format(" cave = ~w\n", [cave])
+    tetris(rks+1, rocks, n, floor, cave, wind)
   end
 
-  def print(cave, n) do
+  def print(cave, n, floor) do
     IO.write("\n")
     IO.write("\n")    
-    for x <- n..1 do
-      :io.format("~2.w  ", [x])
+    for x <- n..(floor+1) do
+      :io.format("~4.w  |", [x])
       for y <- 1..7 do
 	if (free(cave, {x,y}) ) do
-	  IO.write(" ")
+	  IO.write(".")
 	else
 	  IO.write("#")
 	end
@@ -42,11 +46,57 @@ defmodule Day17 do
     end
     IO.write("\n")
   end
-  
 
-  
 
-  def bar({x,y}, n, cave, wind) do
+  def trim(n, floor, cave) do
+    res = Enum.reduce_while(n..(floor+1), cave, fn(i, cave) ->
+      if ( marked(cave, {i,1})) do 
+      case walk({i,1}, cave) do
+	{:ok, k} ->
+	  #:io.format(" found path at ~w, removing from ~w to ~w\n", [i, floor, k])
+	  #print(cave, n, floor)
+	  cave = remove(cave, floor+1, k)
+	  #print(cave, n, floor)	  
+	  {:halt, {k, cave}}
+	{:no, _} ->
+	  {:cont, cave}
+      end
+      else
+	{:cont, cave}
+      end
+    end)
+    case res do
+      {floor, cave} -> {floor, cave}
+      _ -> {floor, cave}
+    end
+  end
+
+  def walk(pos, cave) do
+    walk(pos, MapSet.new([pos]), cave)
+  end
+
+  def walk({x,7}, _path, _cave) do {:ok, x} end
+  def walk({x,y}, path, cave) do
+    res = Enum.reduce_while([{x+1,y},{x,y+1},{x-1,y},{x, y-1}], path, fn({x,y}, path) ->
+      if (marked(cave, {x,y}) and !loop(path, {x,y})) do
+	 path = MapSet.put(path, {x, y})
+	 case walk({x,y}, path, cave) do
+	   {:ok, k} ->
+	     {:halt, {:ok, min(k,x)}}
+	   {:no, path} ->
+	     {:cont, path}
+	 end
+      else
+	{:cont, path}
+      end
+    end)
+    case res do
+      {:ok, k} -> {:ok, k}
+      path -> {:no, path}
+    end
+  end
+  
+  def bar({x,y}, n, floor, cave, wind) do
     ##  the shape 
     ##
     ##    x###
@@ -67,10 +117,9 @@ defmodule Day17 do
 	end
       end
 
-    if (x > 1 and free(cave, {x-1,y}) and free(cave, {x-1,y+1}) and free(cave, {x-1,y+2}) and free(cave, {x-1,y+3}) ) do
-      bar({x-1, y}, n, cave, wind)
+    if ((x - 1 > floor) and free(cave, {x-1,y}) and free(cave, {x-1,y+1}) and free(cave, {x-1,y+2}) and free(cave, {x-1,y+3}) ) do
+      bar({x-1, y}, n, floor, cave, wind)
     else
-
       cave = block(cave, {x,y})
       cave = block(cave, {x,y+1})      
       cave = block(cave, {x,y+2})
@@ -81,7 +130,7 @@ defmodule Day17 do
   end
 
 
-  def cross({x,y}, n, cave, wind) do
+  def cross({x,y}, n, floor, cave, wind) do
     ##  the shape 
     ##
     ##     #
@@ -104,8 +153,8 @@ defmodule Day17 do
 	end
       end
 
-    if (x > 1 and free(cave, {x,y}) and free(cave, {x-1,y+1}) and free(cave, {x,y+2}) ) do
-      cross({x-1, y}, n, cave, wind)
+    if ((x-1 > floor) and free(cave, {x,y}) and free(cave, {x-1,y+1}) and free(cave, {x,y+2}) ) do
+      cross({x-1, y}, n, floor, cave, wind)
     else
 
       cave = block(cave, {x+1,y})
@@ -121,7 +170,7 @@ defmodule Day17 do
   end   
 
 
-  def el({x,y}, n, cave, wind) do
+  def el({x,y}, n, floor, cave, wind) do
     ##  the shape 
     ##
     ##      #
@@ -144,8 +193,8 @@ defmodule Day17 do
 	end
       end
 
-    if (x > 1 and free(cave, {x-1,y}) and free(cave, {x-1,y+1}) and free(cave, {x-1,y+2}) ) do
-      el({x-1, y}, n, cave, wind)
+    if (((x-1) > floor) and free(cave, {x-1,y}) and free(cave, {x-1,y+1}) and free(cave, {x-1,y+2}) ) do
+      el({x-1, y}, n, floor, cave, wind)
     else
 
       cave = block(cave, {x,y})
@@ -160,7 +209,7 @@ defmodule Day17 do
   end   
 
 
-  def rod({x,y}, n, cave, wind) do
+  def rod({x,y}, n, floor, cave, wind) do
     ##  the shape 
     ##
     ##    #
@@ -187,9 +236,9 @@ defmodule Day17 do
 	end
       end
 
-    if (x > 1 and free(cave, {x-1,y}) ) do
+    if (((x-1) > floor) and free(cave, {x-1,y}) ) do
       ## :io.format(" down\n", [])
-      rod({x-1, y}, n, cave, wind)
+      rod({x-1, y}, n, floor, cave, wind)
     else
 
       cave = block(cave, {x,y})
@@ -201,7 +250,7 @@ defmodule Day17 do
     end
   end   
 
-  def block({x,y}, n, cave, wind) do
+  def block({x,y}, n, floor, cave, wind) do
     ##  the shape 
     ##
     ##    ##
@@ -223,8 +272,8 @@ defmodule Day17 do
 	end
       end
 
-    if (x > 1 and free(cave, {x-1,y}) and free(cave, {x-1,y+1}) ) do
-      block({x-1, y}, n, cave, wind)
+    if (((x-1) > floor) and free(cave, {x-1,y}) and free(cave, {x-1,y+1}) ) do
+      block({x-1, y}, n, floor, cave, wind)
     else
 
       cave = block(cave, {x,y})
@@ -237,13 +286,17 @@ defmodule Day17 do
   end   
 
   def rock(i) do
-    rocks = { &bar(&1,&2,&3,&4),
-	      &cross(&1,&2,&3,&4),
-	      &el(&1,&2,&3,&4),
-	      &rod(&1,&2,&3,&4),
-	      &block(&1,&2,&3,&4) }
+    rocks = { &bar(&1,&2,&3,&4,&5),
+	      &cross(&1,&2,&3,&4,&5),
+	      &el(&1,&2,&3,&4,&5),
+	      &rod(&1,&2,&3,&4,&5),
+	      &block(&1,&2,&3,&4,&5) }
     ## :io.format("select ~w\n", [rem(i,5)])
     elem(rocks, rem(i,5))
+  end
+
+  def wind(wind) do
+    {wind, wind}
   end
   
   def jet({[], wind}) do
@@ -253,14 +306,46 @@ defmodule Day17 do
     {jet, {rest, wind}}
   end
 
+  def cave() do
+    Map.new()
+  end
+
+  def marked(cave, {x,y}) do
+    case Map.get(cave, x) do
+      nil -> false
+      row ->
+	MapSet.member?(row,y)
+    end
+  end    
+  
   def free(cave, {x,y}) do
-    !(MapSet.member?(cave,{x,y}))
+    case Map.get(cave, x) do
+      nil -> true
+      row ->
+	!(MapSet.member?(row,y))
+    end
+  end
+
+  def remove(cave, frm, to) do
+    Enum.reduce(frm..to, cave, fn(i, cave) -> Map.delete(cave, i) end)
+  end
+  
+  
+  def filled(cave, x) do
+    case Map.get(cave, x) do
+      nil ->
+	false
+      row ->
+	MapSet.size(row) == 7
+    end
   end
 
   def block(cave, {x,y}) do
-    MapSet.put(cave, {x,y})
+    Map.update(cave, x,  MapSet.new([y]), fn(row) -> MapSet.put(row, y) end)
   end
-  
-    
+
+  def loop(path, {x,y}) do
+    MapSet.member?(path, {x,y})
+  end
   
 end
