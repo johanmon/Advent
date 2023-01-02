5d15ee5d4e0c7cf14d96ca0ab76f8d33aac339ba
Johan Montelius <johanmon@kth.se>
2023-01-02 09:19:10 +0100

    Day 19 still not done :-(

--- 2022/day19.ex
+++ 2022/day19.ex
@@ -15,25 +15,53 @@ defmodule Day19 do
       Enum.reduce(0, fn({g,i,_}, a) -> g*i+a end)
   end  
 
-  def debug(k, t) do
-    debug(k, t, List.duplicate(:*, t))
+  def debug(k, t, brk) do
+    debug(k, t, brk, List.duplicate(:*, t))
   end
   
-  def debug(k, t, path) do
+  def debug(k, t, brk, path) do
     {:ok, blue} = sample() |>
       Enum.map(fn(r) -> parse(r) end) |>
-      Enum.fetch(k)
-    quality(blue, t, path)
-  end
-
-  def quality(blue, t) do
-    quality(blue, t, List.duplicate(:*, t))
+      Enum.fetch(k-1)
+    quality(blue, t, brk, path)
   end
   
-  def quality({id, specs}, t, nxts) do
+
+  def iterative(k) do
+    {:ok, blue} = sample() |>
+      Enum.map(fn(r) -> parse(r) end) |>
+      Enum.fetch(k-1)
+    iterative(28, blue, 0)
+  end
+
+  
+  def iterative(32, blue, brk) do
+    {brk, id, path} = quality(blue, 32, brk, List.duplicate(:*, 32))
+    :io.format("iterative: t = ~w,  brk = ~w\n", [32, brk])
+    {brk, id, path}
+  end
+  def iterative(t, blue, brk) do
+    {brk, _, _} = quality(blue, t, brk, List.duplicate(:*, t))
+    :io.format("iterative: t = ~w,  brk = ~w\n", [t, brk])
+    iterative(t+1, blue, brk)
+  end
+
+
+  def limit(k, t, brk)  do
+    {:ok, blue} = sample() |>
+      Enum.map(fn(r) -> parse(r) end) |>
+      Enum.fetch(k-1)
+    quality(blue, t, brk, List.duplicate(:*, t))
+  end
+  
+  
+  def quality(blue, t) do
+    quality(blue, t, 0, List.duplicate(:*, t))
+  end
+  
+  def quality({id, specs}, t, brk, nxts) do
     specs = specs(specs)
-    {{geodes,path}, _} = dynamic(t, 0, {1,0,0,0}, {0,0,0,0}, specs, Map.new(), nxts)
-    :io.format(" ~w  ~w \n", [id, geodes])
+    {{geodes,path}, _} = dynamic(t, brk, {1,0,0,0}, {0,0,0,0}, specs, Map.new(), nxts)
     {geodes, id, path}
   end
 
@@ -49,6 +77,7 @@ defmodule Day19 do
 	mem = Map.put(mem, {t, robots, resources}, max)
 	{max, mem}
       max ->
+	#:io.format(" break: ~w,  ~w,  ~w   max ~w\n", [t, robots, resources, max])
 	{max, mem}
     end
   end
@@ -62,25 +91,31 @@ defmodule Day19 do
 	search(t, brk, robots, resources, specs, mem, nxts)
       end
       2 -> if ( ((geoder*2) < (brk-geode)) or (((geoder*2) < (brk-geode-1)) and clay >= cc and obsidian >= oc) ) do
+	#:io.format(" break : t = ~w, brk = ~w  geode = ~w\n", [t, brk, geode])
       	{{geode, []}, mem}
       else
 	search(t, brk, robots, resources, specs, mem, nxts)
       end
       3 -> if ( ((geoder*3) < (brk-geode-1)) or (((geoder*3) < (brk-geode-3)) and clay >= cc and obsidian >= oc) ) do
+	#:io.format(" break : t = ~w, brk = ~w  geode = ~w\n", [t, brk, geode])
       	{{geode, []}, mem}
       else
 	search(t, brk, robots, resources, specs, mem, nxts)
       end
       4 -> if ( ((geoder*4) < (brk-geode-3)) or  (((geoder*4) < (brk-geode-6)) and clay >= cc and obsidian >= oc)     ) do
+	#:io.format(" break : t = ~w, brk = ~w  geode = ~w\n", [t, brk, geode])
       	{{geode, []}, mem}
       else
 	search(t, brk, robots, resources, specs, mem, nxts)
       end
-      t -> if ( ((geoder*t) < (brk-geode-(Enum.sum(1..(t-2))))) or (((geoder*t) < (brk-geode-Enum.sum(1..(t-1)) )) and clay >= cc and obsidian >= oc))  do  
-      	{{geode, []}, mem}
-      else
-      	search(t, brk, robots, resources, specs, mem, nxts)
-      end
+      #_ ->
+      #  search(t, brk, robots, resources, specs, mem, nxts)
+       t ->  if ( ((geoder*t) < (brk-geode-(Enum.sum(1..(t-2))))) or (((geoder*t) < (brk-geode-Enum.sum(1..(t-1)) )) and clay >= cc and obsidian >= oc))  do  
+        	#:io.format(" break : t = ~w, brk = ~w  geode = ~w\n", [t, brk, geode])
+        {{geode, []}, mem}
+       else
+        	search(t, brk, robots, resources, specs, mem, nxts)
+       end
     end
     
   end
@@ -89,41 +124,62 @@ defmodule Day19 do
 
     all = []
     
-    {all, brk, mem} = if( afford(orec, resources) and !afford(geodec, resources) and next(nxt,:ore) ) do
+    {all, brk, mem} = if( afford(orec, resources) and next(nxt,:ore) ) do
       {{max, path}, mem} = dynamic(t-1, brk, build(:ore, robots), collect(robots, orec, resources), specs, mem, nxts)
-      {[{max, [:ore|path]}|all], max(max,brk), mem}
+      if max != 0 do
+	{[{max, [:ore|path]}|all], max(max,brk), mem}
+      else
+	{all, brk, mem}
+      end
     else
       {all, brk, mem}
     end
-    {all, brk, mem} = if( afford(clayc, resources) and !afford(geodec, resources) and next(nxt,:clay)) do
+    {all, brk, mem} = if( afford(clayc, resources) and  next(nxt,:clay)) do
       {{max,path}, mem} = dynamic(t-1, brk, build(:clay, robots), collect(robots, clayc, resources), specs, mem, nxts)
-      {[{max, [:clay|path]}|all], max(max,brk), mem}
+      if max != 0 do
+	{[{max, [:clay|path]}|all], max(max,brk), mem}
+      else
+	{all, brk, mem}
+      end
     else
       {all, brk, mem}
     end
     {all,brk,mem} = if( afford(obsidianc, resources) and  next(nxt,:obsidian)) do
       {{max,path}, mem} = dynamic(t-1, brk, build(:obsidian, robots), collect(robots, obsidianc, resources), specs, mem, nxts)
-      {[{max,[:obsidian|path]}|all], max(max,brk), mem}
+      if max != 0 do
+	{[{max,[:obsidian|path]}|all], max(max,brk), mem}
+      else
+	{all, brk, mem}
+      end
     else
       {all, brk, mem}
     end
     {all,brk,mem} = if( afford(geodec, resources) and next(nxt,:geode)) do
       {{max,path}, mem} = dynamic(t-1, brk, build(:geode, robots), collect(robots, geodec, resources), specs, mem, nxts)
-      {[{max, [:geode|path]}|all], max(max,brk), mem}
+      if max != 0 do
+	{[{max, [:geode|path]}|all], max(max,brk), mem}
+      else
+	{all, brk, mem}
+      end
     else
       {all, brk, mem}
     end
     {all, _, mem} = if( !afford(geodec, resources) and next(nxt,:na) ) do
       {{max,path}, mem} = dynamic(t-1, brk, robots, collect(robots, resources), specs, mem, nxts)
-      {[{max, [:na|path]}|all], max(max,brk), mem}
+      if max != 0 do
+	{[{max, [:na|path]}|all], max(max,brk), mem}
+      else
+	{all, brk, mem}
+      end
     else
       {all, brk, mem}
     end
+
     if (all == []) do
-      :io.format("\n\n t = ~w nxt = ~w  , nxts = ~w resources = ~w  specs = ~w\n", [t, nxt, nxts, resources, specs])
-      throw(:error)
+       ## :io.format("\n\n t = ~w nxt = ~w  , nxts = ~w resources = ~w  specs = ~w\n", [t, nxt, nxts, resources, specs])
+       {{0,[]}, mem}
     else
-      {Enum.max(all, fn({x,_},{y,_}) -> x > y end), mem}
+       {Enum.max(all, fn({x,_},{y,_}) -> x > y end), mem}
     end
   end
 
@@ -165,6 +221,22 @@ defmodule Day19 do
 
   def cost([ore: o, clay: c]) do {o,c,0} end
   def cost([ore: o, obsidian: s]) do {o,0,s} end
+
+
+  def solution() do
+    {[:na,       :na,    :na,    :na,       :ore,   :na,       :clay,     :clay,  :clay,     :clay,
+     :clay,     :clay,  :clay,  :obsidian, :na,    :obsidian, :obsidian, :na,    :obsidian, :geode,
+     :obsidian, :geode, :geode, :geode,    :na,    :geode,    :geode,    :na,    :geode,    :geode,
+      :geode,    :na],
+     [:na,   :na,   :ore, :na, :ore, :clay, :clay, :clay, :clay, :clay,
+      :obsidian, :clay, :obsidian, :obsidian, :obsidian, :ore, :obsidian, :obsidian, :geode, :geode,
+      :obsidian, :geode, :obsidian, :geode, :geode, :clay, :geode, :geode,  :clay, :geode,
+      :geode, :clay],
+     [],
+     [],
+     []}
+  end
+  
   
   def input() do
     File.stream!("day19.csv")
@@ -172,9 +244,16 @@ defmodule Day19 do
 
   def sample() do
     ["Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
-     "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."]
+     "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
+     "Blueprint 3: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 14 clay. Each geode robot costs 3 ore and 16 obsidian.",
+     "Blueprint 4: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 19 clay. Each geode robot costs 2 ore and 12 obsidian.",
+     "Blueprint 5: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 9 clay. Each geode robot costs 4 ore and 16 obsidian."
+    ]
   end
 
+  
+
+  
   def parse(line) do
     [blue | specs] = String.split(String.trim(line), [":", "."])
     [_,nr] =  String.split(blue, [" "])
